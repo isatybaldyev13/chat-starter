ef0e58d5bc58e1d556ced68b579636186eceb45b
Cucumber Rick <150014886+Cucumberchik@users.noreply.github.com>
2024-04-28 12:14:40 +0600

    Delete src directory

--- src/firebase/firestore.js
+++
@@ -1,7 +0,0 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db } from ".";
-
-
-export const createMessage = async message => {
-  await addDoc(collection(db, "kattar"), message);
-}
