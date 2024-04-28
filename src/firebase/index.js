23da9f8da9d893122793ce7fdf788e25d690c0e8
isatybaldyev13 <isatybaldyev@gmail.com>
2024-04-28 09:39:52 +0600

    UI created

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,15 @@
-import { getAuth } from '@firebase/auth';
-import { initializeApp } from 'firebase/app';
+import { getAuth } from "@firebase/auth";
+import { initializeApp } from "firebase/app";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWTqBkKWj6kGuuDtuWOmfMGTLerBkgbuM",
-  authDomain: "fir-app-eff25.firebaseapp.com",
-  projectId: "fir-app-eff25",
-  storageBucket: "fir-app-eff25.appspot.com",
-  messagingSenderId: "63924157201",
-  appId: "1:63924157201:web:86ac6a9496d94eb90caab0",
-  measurementId: "G-LV7G7ZST4J"
+  apiKey: "AIzaSyCy6a5a5_fdSkfcM848VGF5NP_3ZAUMSQE",
+  authDomain: "chat-app-c2999.firebaseapp.com",
+  projectId: "chat-app-c2999",
+  storageBucket: "chat-app-c2999.appspot.com",
+  messagingSenderId: "328912180692",
+  appId: "1:328912180692:web:294cbe2a0e0d5fcfcb7241",
+  measurementId: "G-2G2F5F3GV7",
 };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,25 +1,43 @@
-import { Box, Container, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useAuth } from "../hooks/useAuth";
 import Header from "../components/header";
-import { Add } from "@mui/icons-material";
+import { Add, Send } from "@mui/icons-material";
+import { useState } from "react";
 
 const Dashboard = () => {
   const { authUser } = useAuth();
+  const [message, setMessage] = useState("");
 
-  console.log("authUser ", authUser);
+  const onSendMessage = () => {};
 
   return (
-    <>
+    <Box display="flex" flexDirection="column" height="100vh">
       <Header />
-      <Container>
-        <Box display={"flex"} alignItems={"center"} pt={2} gap={1}>
-          <Typography variant="h6">Expenses</Typography>
-          <IconButton onClick={() => {}}>
-            <Add />
-          </IconButton>
+      <Box display={"flex"} flex={1}>
+        <Box display={"flex"} sx={{ background: "yellow" }} flex={1}></Box>
+        <Box display={"flex"} flex={3} flexDirection={"column"}>
+          <Box display={"flex"} flex={1} sx={{ background: "purple" }}></Box>
+          <Box display={"flex"}>
+            <TextField
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              fullWidth
+              placeholder="message"
+            />
+            <Button onClick={onSendMessage}>
+              <Send />
+            </Button>
+          </Box>
         </Box>
-      </Container>
-    </>
+      </Box>
+    </Box>
   );
 };
 
